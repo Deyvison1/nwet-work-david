df87c1d28a28e2c538b86078c646310d2a5d84bd
Deyvison1 <deyvison.oliveira>
2024-11-10 12:48:43 -0300

    Ajustes cors

---
+++ src/main/java/api/networkn/config/CorsFilter.java
@@ -0,0 +1,48 @@
+package api.networkn.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+		// vazio de proposito
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse resp = (HttpServletResponse) response;
+		HttpServletRequest reque = (HttpServletRequest) request;
+		resp.setHeader("Access-Control-Allow-Origin", "*");
+		resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS,DELET,PUT");
+		resp.setHeader("Access-Control-Allow-Max-Age", "3600");
+		resp.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization");
+
+		if ("OPTIONS".equalsIgnoreCase(reque.getMethod())) {
+			resp.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+}
